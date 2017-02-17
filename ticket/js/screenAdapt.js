0e0940e11b5e324f3debc40d381c374c912baa8e
ShariseMo <1009708910@qq.com>
2017-02-17 13:59:17 +0800

    入场券【移动版h5】

---
+++ ticket/js/screenAdapt.js
@@ -0,0 +1,12 @@
+(function () {
+    var b = document.documentElement,
+    a = function () {
+        var a = b.getBoundingClientRect().width;
+        b.style.fontSize = (100/750) * (a >= 750 ? 750 : a) + "px"
+    }, c = null;
+    window.addEventListener("resize", function () {
+        clearTimeout(c);
+        c = setTimeout(a, 300)
+    });
+    a();
+})();
